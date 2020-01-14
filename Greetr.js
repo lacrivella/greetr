ee2c5f07da9e018d01d093fad4e70e706b61c6e0
Leigh-Ann Crivella <la.crivella.2@gmail.com>
2020-01-14 15:46:05 -0800

    function constructor that builds an object and gives it three properties

--- Greetr.js
+++ Greetr.js
@@ -1,3 +1,13 @@
 (function(global, $) {
-  
+
+  var Greetr = function(firstname, lastname, language) {
+    return new Greetr.init(firstname, lastname, language);
+  }
+  Greetr.init = function(firstname, lastname, language) {
+    
+    this.firstname = firstname || '';
+    this.lastname = lastname || '';
+    this.language = language || 'en';
+
+  }
 }(window, jQuery));
