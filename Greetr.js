2b717a67a645bfb0bf902855e1ebf6481707c7a8
Leigh-Ann Crivella <la.crivella.2@gmail.com>
2020-01-14 16:25:26 -0800

    added variables that won't change or aren't exposed

--- Greetr.js
+++ Greetr.js
@@ -4,6 +4,23 @@
     return new Greetr.init(firstname, lastname, language);
   }
 
+  var supportedLangs = ['en', 'es'];
+
+  var greetings = {
+    en: 'Hello',
+    es: 'Hola'
+  };
+
+  var formalGreetings = {
+    en: 'Greetings',
+    es: 'Saludos'
+  };
+
+  var logMessages = {
+    en: 'Logged in',
+    es: 'Inció sesión'
+  }
+
   Greetr.prototype = {};
 
   Greetr.init = function(firstname, lastname, language) {
