2090483cc1c51fe77787baef398c01ce500c333c
Leigh-Ann Crivella <la.crivella.2@gmail.com>
2020-01-14 17:44:36 -0800

    added some jQuery

--- Greetr.js
+++ Greetr.js
@@ -72,6 +72,28 @@
       this.language = lang;
       this.validate();
       return this;
+    },
+
+    HTMLGreeting: function(selector, formal) {
+      if(!$) {
+        throw 'jQuery not loaded';
+      }
+      if(!selector) {
+        throw 'Missing jQuery selector';
+      }
+
+      var msg;
+      if(formal) {
+        msg = this.formalGreeting();
+      }
+      else {
+        msg = this.greeting();
+      }
+
+      $(selector).html(msg);
+
+      // makes it chainable
+      return this;
     }
 
   };
