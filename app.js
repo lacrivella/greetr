b065c35a697db3bb425fe22d355026ebbbeb6af0
Leigh-Ann Crivella <la.crivella.2@gmail.com>
2020-01-14 18:43:08 -0800

    added jQuery functionality to our framework/mini library

--- app.js
+++ app.js
@@ -1,2 +1,20 @@
+// gets a new object (the architecture allows us to not have to use the 'new' keyword here)
 var g = G$('Roach', 'the Horse');
+
+// using our chainable methods
 g.greet().setLang('es').greet(true);
+
+// let's use our object on the click of the login button
+$('#login').click(function() {
+
+  // creates new 'Greetr' object using function
+  var loginGrtr = G$('Roach', 'the Horse');
+
+  // hide the UI login button
+  $('#logindiv').hide();
+
+  // set language based on what's chosen in the select box
+  // chain a method that updates the h1 (formal because true)
+  // log to console
+  loginGrtr.setLang($('#lang').val()).HTMLGreeting('#greeting', true).log();
+});
